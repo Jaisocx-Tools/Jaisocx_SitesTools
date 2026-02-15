bae19d8537a9b40be9fe5a6d926ac840a16a9b11
ILLIA POLIANSKYI <polianskii.ilia@gmail.com>
2026-02-15 22:55:07 +0100

    improved gitignore

---
+++ sites_tools/css_tools/pages/responses/exception_responses/http_404_exception/src/index.ts
@@ -0,0 +1,8 @@
+// these imports of styles enable these styles and themes via import atatement of this package in another .js file, 
+// when built with webpack and using the webpackAliases of this package.
+
+
+import "@jaisocx__http_exception_404__MediaAndStyles/http_exception_404-wbp.css";
+
+
+
--- sites_tools/css_tools/pages/responses/exception_responses/https_404_exception/transpiled/ESNext/index.d.ts
+++
@@ -1,4 +0,0 @@
-import "@jaisocx-css-clean-start-MediaAndStyles/clean-start-main-webpack.css";
-declare const _default: "@jaisocx-css-clean-start-MediaAndStyles/clean-start-main-webpack.css";
-export default _default;
-//# sourceMappingURL=index.d.ts.map
